500593d0a2cd74276071e61bbfd606b3ce428ae2
Rohit Thorat <thorat.rohit95@gmail.com>
2020-09-20 17:41:36 +0530

    added collection oveview component

---
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect'
+
+import CollectionPreview from '../collection-preview/collection-preview.component';
+import './collections-overview.styles.scss';
+import { selectCollections } from '../../redux/shop/shop.selectors';
+
+const CollectionsOverview = ({ collections }) => (
+    <div className='collections-overview'>
+        {
+            collections.map(({ id, title, routeName, items }) => (
+                <CollectionPreview key={id} title={title} items={items} />
+            ))
+        }
+    </div>
+);
+
+const mapStateToProps = createStructuredSelector({
+    collections: selectCollections
+})
+
+export default connect(mapStateToProps)(CollectionsOverview);
