0412932deebee8eeb495301d3141f062249a26fb
Rohit Thorat <thorat.rohit95@gmail.com>
2020-09-26 16:00:09 +0530

    add redux saga

---
+++ src/redux/shop/shop.sagas.js
@@ -0,0 +1,22 @@
+import { takeEvery, call, put } from 'redux-saga/effects';
+import { convertCollectionsSnapshotToMap, firestore } from '../../firebase/firebase.utils';
+import ShopActionTypes from  './shop.types';
+import { fetchCollectionsSuccess, fetchCollectionsFailure } from './shop.actions'
+
+export function* fetchCollectionsAsync() {
+    yield console.log('I am fired.');
+    try{
+      const collectionRef = firestore.collection('collections');
+      const snapshot = yield collectionRef.get();
+      //call takes first argyment as ducntion and all subsequesnt are argument for thise functions
+      const collectionsMap = yield call(convertCollectionsSnapshotToMap, snapshot);
+      //put is saga effect for creating dispatch
+      yield put(fetchCollectionsSuccess(collectionsMap));
+    }catch(error) {
+      yield put(fetchCollectionsFailure(error.message));
+    }
+}
+
+export function* fetchCollectionsStart() {
+    yield takeEvery(ShopActionTypes.FETCH_COLLECTIONS_START, fetchCollectionsAsync)
+}
